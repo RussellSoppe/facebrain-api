d08294d87b5ff99a0ec4361c4c80b26def38bc59
russellicious <42648750+russellicious@users.noreply.github.com>
2018-11-05 11:14:58 -0600

    adding port

--- server.js
+++ server.js
@@ -47,5 +47,5 @@ app.put('/image', (req, res)=>{image.handleImage(req, res, db)})
 app.post('/imageurl', (req, res)=>{image.handleApiCall(req, res)})
 
 
-app.listen(3000)
+app.listen(process.env.PORT)
 
